31dc744885b2028184b976de76d4489658d3de6d
billizzard <billizzard@mail.ru>
2018-11-18 02:42:55 +0300

    Create skeleton for WP plugins with PSR2 standarts

---
+++ includes/MyusefulpluginActivator.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since 1.0.0
+ * @package Myusefulplugin
+ * @subpackage Myusefulplugin/includes
+ * @author AuthorName <AuthorEmail@email.com>
+ */
+class MyusefulpluginActivator
+{
+    /**
+     * Short Description. (use period)
+     *
+     * Long Description.
+     *
+     * @since 1.0.0
+     */
+    public static function activate()
+    {
+    }
+}
